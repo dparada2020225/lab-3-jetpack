42a8079e94265c81123c308a85517033e57d5e54
yosemm <140467686+yosemm@users.noreply.github.com>
2025-08-02 08:01:56 -0600

    Add MyPokedex project

---
+++ MyPokedex/app/src/main/java/com/uvg/mypokedex/ui/features/home/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.uvg.mypokedex.ui.features.home
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.uvg.mypokedex.ui.theme.AppTypography
+import com.uvg.mypokedex.ui.theme.MyPokedexTheme
+import com.uvg.mypokedex.ui.theme.bodyFontFamily
+import com.uvg.mypokedex.ui.theme.displayFontFamily
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyPokedexTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier,
+        style = AppTypography.displayLarge
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyPokedexTheme {
+        Greeting("Android")
+    }
+}
